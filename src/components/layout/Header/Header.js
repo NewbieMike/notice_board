65d4df64d5e5c0d7b2e4baa583bb3fb2c94a4c44
Michał Micherda <mmicherdakr@gmail.com>
2021-08-20 12:08:46 +0200

    Add header with login/logout function

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -5,15 +5,74 @@ import clsx from 'clsx';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+// import HomeIcon from '@material-ui/icons/Home';
+// import { green } from '@material-ui/core/colors';
+// import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import FormGroup from '@material-ui/core/FormGroup';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import { Button } from '@material-ui/core';
 import styles from './Header.module.scss';
+import { makeStyles } from '@material-ui/core';
+import AssignmentLateSharpIcon from '@material-ui/icons/AssignmentLateSharp';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>Header</h2>
-    {children}
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(1),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Component = ({className, children}) => {
+  const classes = useStyles();
+  const [login, setLogin] = React.useState();
+  const handleChange = (event) => {
+    setLogin(event.target.checked);
+  };
+  return (
+    <div className={clsx(className, styles.root)}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" className={classes.title}>
+            Your local notice board
+          </Typography>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={login}
+                onChange={handleChange}
+                aria-label='login switch'
+              />
+            }
+            label={login ? 'Logout' : 'Login'}
+          />
+          {!login ? '' : 
+            <Button edge="start" className={classes.menuButton} color="inherit" aria-label="menu" href='/'>
+             My Announcements <AssignmentLateSharpIcon/>
+            </Button>
+          }
+
+          {!login ? 
+            <Button color="inherit" href='https://google.com'>Login <MeetingRoomIcon /></Button> : <Button color="inherit" href='/'>Logout <MeetingRoomIcon /></Button>
+          }
+          
+        </Toolbar>
+      </AppBar>
+      {children}
+    </div>
+  );
+  
+};
 
 Component.propTypes = {
   children: PropTypes.node,
